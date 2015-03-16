94caefc30b2f10e15a7026b6328809a2919cb8fb
Armin Akhavan <armin.akhavan@gmail.com>
2015-03-16 13:40:04 -0400

    fixing dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -214,6 +214,6 @@ module.exports = function (grunt) {
         grunt.task.run(['coffee:testcoffee', 'connect:test', 'shell:mocha-phantomjs', 'watch:tests']);
     });
     grunt.task.registerTask('build', 'creates optimized distribution', function () {
-        grunt.task.run(['dev', 'targethtml:dist', 'shell:buildRequire', 'copy:requireBuilt', 'cssmin:minify', 'express:dist', 'open:build', 'watch:indextemplate']);
+        grunt.task.run(['dev', 'targethtml:dist', 'shell:buildRequire', 'cssmin:minify', 'express:dist']);
     });
 };
